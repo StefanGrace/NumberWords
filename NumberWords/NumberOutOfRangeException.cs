70d6a51d0dc0ca8d67e0e2e4272fa8c2e353475b
StefanGrace <stefangrace@hotmail.co.nz>
2022-12-20 03:05:42 +1300

    Add files via upload

---
+++ NumberWords/NumberOutOfRangeException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberWords
+{
+    class NumberOutOfRangeException : Exception
+    {
+        private string errorMessage = "Number must be less than 10^15 and more than -(10^15).";
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return errorMessage;
+            }
+        }
+
+    }
+}
